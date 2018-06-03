7419fcc5b6308da0b8d2884172dc466fd28a990e
winechen9 <zheyingchen9@gmail.com>
2018-06-03 12:25:09 -0400

    test

---
+++ src/main/java/demo/rest/RunningUploadRestController.java
@@ -0,0 +1,17 @@
+package demo.rest;
+
+import demo.service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RunningUploadRestController {
+    //dependency injection
+    private LocationService locationService;
+
+    @Autowired
+    public RunningUploadRestController(LocationService locationService) {
+        this.locationService = locationService;
+    }
+    //xx
+}
